5ba2e4209c010a9012d5bc216f9de917839e03cf
Vincent <vincent@getseoshop.com>
2018-07-06 17:01:12 +0200

    wwiiiiip

--- src/app/Http/Controllers/PaymentController.php
+++ src/app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
@@ -18,6 +19,8 @@ class PaymentController extends Controller
 
     public function pay($transactionId)
     {
+        $results = DB::select("SELECT * FROM users");
+
         return view('pay', ['transaction_id' => $transactionId]);
     }
 
